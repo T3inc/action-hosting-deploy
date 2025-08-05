fdf720197118ca9e1e96a1db19c0a7915d9399cb
Clay Houllion <houllion@gmail.com>
2025-08-05 18:15:16 -0500

    Revert "debug"

--- src/deploy.ts
+++ src/deploy.ts
@@ -163,7 +163,7 @@ export async function deployProductionSite(
       "--only",
       `hosting${
         target ? ":" + target : ""
-      },functions,storage,firestore --debug`,
+      },functions,storage,firestore`,
     ],
     projectId,
     gacFilename,
